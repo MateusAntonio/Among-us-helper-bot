f6df7d3eedd79f5b8976b7fe442ed0288381ba3d
mateus Antônio Pereira Gomes <mateus@paytime.com.br>
2020-10-08 20:13:20 -0300

    Removing token

--- src/setup.js
+++ src/setup.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const { token } = require('../config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.login(token);
+client.login(process.env.BOT_TOKEN);
 
 client.once('ready', () => {
   console.log('ready!');
